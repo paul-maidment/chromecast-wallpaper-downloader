f5f598319d9ff437620dc555fff0ce14a21ab24c
Paul Maidment <paulmaidmentuk@gmail.com>
2021-10-01 17:34:27 +0100

    Write directly to Pictures directory of the logged in user

--- main.py
+++ main.py
@@ -1,11 +1,14 @@
 import json
 import requests
 import os
+from pathlib import Path
 from time import sleep
 
 data = requests.get("https://raw.githubusercontent.com/dconnolly/chromecast-backgrounds/master/backgrounds.json")
 backgrounds_json = data.content
 backgrounds_dict = json.loads(backgrounds_json)
+
+Path(os.path.expanduser('~') + "/Pictures/chromecast-backgrounds").mkdir(parents=True, exist_ok=True)
 
 i = 1
 for item in backgrounds_dict:
@@ -14,7 +17,9 @@ for item in backgrounds_dict:
     filename, extension = os.path.splitext(item['url'])
 
     if len(response.content) > 5000:
-        of = open('output/background-' + str(i) + "." + extension, "wb")
+        of = open(
+            os.path.expanduser('~')
+            + "/Pictures/chromecast-backgrounds/background-" + str(i) + "." + extension, "wb")
         of.write(response.content)
         i += 1
         sleep(0.25)
